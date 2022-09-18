13d42537f310c200c7576d2e3ad35a752091f665
Kenneth Ekman <bkekman@gmail.com>
2022-09-18 14:09:00 +0200

    Improved layout test app

--- src/gedcomApp.tsx
+++ src/gedcomApp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { LocalTreeBackend } from 'simple-family-tree-model';
-import { createFamilyLayout, RootLayout, generateLayout } from 'simple-family-tree-layout'
+import { LocalTreeBackend, ProfileLink } from 'simple-family-tree-model';
+import { createFamilyLayout, RootLayout, generateVerticalTreeLayout, generateProfileList } from 'simple-family-tree-layout'
 //import { UncontrolledReactSVGPanZoom } from 'react-svg-pan-zoom'
 import { decodeGedcom } from './decode-gedcom';
 
@@ -10,6 +10,8 @@ interface FamilyTreeState {
   tree: LocalTreeBackend,
   filename: string,
   fileDecoded:boolean,
+  layoutCreated: boolean,
+  focusProfile:string,
   layout: RootLayout
 }
 
@@ -33,41 +35,54 @@ async function buildTreeFromFile(tree: LocalTreeBackend, filename: string): Prom
   return false;
 }
 
-
 export class FamilyTreeComponent extends React.Component<{},FamilyTreeState> {
+  //filename:'../export-BloodTree-2018.ged',
+  //filename:'../555sample.ged',
   state: FamilyTreeState = {
     tree: new LocalTreeBackend(),
     filename:'../555sample.ged',
     fileDecoded:false,
+    layoutCreated: false,
+    focusProfile:"",
     layout: new RootLayout()
   };
 
   private async handleDecode(): Promise<boolean> {
+    console.log("gedcom decode start");
     const result = await buildTreeFromFile(this.state.tree, this.state.filename);
     this.setState({fileDecoded: true})
-    console.log(result);
-    const focusProfile = this.state.tree.getRootProfile();
-    if (focusProfile !== undefined) {
-      console.log(focusProfile);
-      this.setState({layout: createFamilyLayout(this.state.tree, focusProfile, 2, 2)})
-      return true;
-    }
-    console.log("decode failed",result);
-    return false;
+    console.log("gedcom decode done", result);
+    return true;
   }
 
 render() {
   if (!this.state.fileDecoded) {
     this.handleDecode();
   }
-  const focusProfile = this.state.tree.getRootProfile();
-  if (focusProfile !== undefined) {
+  let focusProfileLocal = this.state.tree.getRootProfile();
+  console.log(window.location.href);
+  if (window.location.href.includes("profile")) {
+    let url = window.location.href;
+    let profilestring = url.substring(url.lastIndexOf('/')+1);
+    console.log(profilestring);
+    focusProfileLocal = new ProfileLink(profilestring);
+    if (focusProfileLocal.itemLink !== this.state.focusProfile) {
+      this.setState({layoutCreated: false})
+      this.setState({focusProfile: focusProfileLocal.itemLink})
+    }
+    //this.state.layout.anchorProfile = profilestring;
+  }
+  if (focusProfileLocal !== undefined) {
+    if (!this.state.layoutCreated) {
+      this.setState({layout: createFamilyLayout(this.state.tree, focusProfileLocal, 2, 2)})
+      this.setState({layoutCreated: true})
+    }
     return (
-        <div>
-          A tree...
-            <svg width="800" height="800">
-            {generateLayout(this.state.layout)}
-            </svg>
+        <div style={{display: "grid", gridTemplateColumns: "30% 70%"}}>
+            <div>{generateProfileList(this.state.tree)}</div>
+            <div><svg width="800" height="800">
+            {generateVerticalTreeLayout(this.state.layout)}
+            </svg></div>
         </div>
     )
   } else {
